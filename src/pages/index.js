95773f320d4bddc716f2b615c0d920e8f2a2e476
michaeldremy <michaeldremy@gmail.com>
2023-02-23 09:54:48 -0800

    [DocSearch] Added docSearch package and code snippet for Algolia application process.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router'
+import { DocSearch } from '@docsearch/react'
+
+import '@docsearch/css'
 
 // Redirects users from the base route '/'
 export default function Home() {
   return (
-    <Redirect to="/documentation/category/frequently-asked-questions" />
-  );
+    <>
+      <Redirect to='/documentation/category/frequently-asked-questions' />
+      <DocSearch
+        appId='YOUR_APP_ID'
+        indexName='YOUR_INDEX_NAME'
+        apiKey='YOUR_SEARCH_API_KEY'
+      />
+    </>
+  )
 }

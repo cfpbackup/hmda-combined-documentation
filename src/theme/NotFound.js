7d96f379a63677c723e1d3198c89be2192ea0ad1
michaeldremy <michaeldremy@gmail.com>
2023-03-20 11:07:58 -0700

    [Swizzle] Swizzled NotFound page and updated the content of that page. Addtionally Swizzled Algolia search component.

---
+++ src/theme/NotFound.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Translate, { translate } from '@docusaurus/Translate'
+import { PageMetadata } from '@docusaurus/theme-common'
+import Layout from '@theme/Layout'
+import SearchBar from './SearchBar'
+
+export default function NotFound() {
+  let urlLocation = window.location.pathname
+  return (
+    <>
+      <PageMetadata
+        title={translate({
+          id: 'theme.NotFound.title',
+          message: 'Page Not Found',
+        })}
+      />
+      <Layout>
+        <main className='container margin-vert--xl'>
+          <div className='row'>
+            <div className='col col--6 col--offset-3'>
+              <h1 className='hero__title'>
+                <Translate
+                  id='theme.NotFound.title'
+                  description='The title of the 404 page'
+                >
+                  Page Not Found
+                </Translate>
+              </h1>
+              <div>
+                <p style={{ fontWeight: 600 }}>
+                  URL: <span style={{ fontWeight: 500 }}>{urlLocation}</span>
+                </p>
+                <p>
+                  The above URL was not able to direct you to a specific
+                  document.
+                </p>
+              </div>
+              <p>
+                <Translate
+                  id='theme.NotFound.p1'
+                  description='The first paragraph of the 404 page'
+                >
+                  Please use the search bar below to search for the content you
+                  were looking for.
+                </Translate>
+              </p>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignContent: 'center',
+                  marginBottom: '1rem',
+                }}
+              >
+                <SearchBar />
+              </div>
+              <p>
+                <Translate
+                  id='theme.NotFound.p2'
+                  description='The 2nd paragraph of the 404 page'
+                >
+                  Please contact the owner of the site that linked you to the
+                  original URL and let them know their link is broken.
+                </Translate>
+              </p>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </>
+  )
+}

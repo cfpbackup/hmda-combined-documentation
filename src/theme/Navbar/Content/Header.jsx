934a8ce0c5e1c36552fb43c63257338400731f37
ojbravo <ojbravo@gmail.com>
2023-04-28 08:16:46 -0600

    Add dynamic links + algolia search to uswds navbar

--- src/theme/Navbar/Content/Header.jsx
+++ src/theme/Navbar/Content/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { defaultLinks } from './links'
 
 import '../uswds/css/styles.css'
 import FFIEC_Logo from './images/ffiec-logo.svg'
@@ -6,8 +8,11 @@ import CloseBtn from '../uswds/img/usa-icons/close.svg'
 
 import BannerUSA from './BannerUSA'
 
+import SearchBar from '@theme/SearchBar';
+import NavbarSearch from '@theme/Navbar/Search';
 
-const Header = () => {
+
+const Header = ({links = defaultLinks}) => {
   return (
     <div>
       <BannerUSA />
@@ -27,108 +32,57 @@ const Header = () => {
             <button type="button" className="usa-nav__close">
               <CloseBtn />
             </button>
-            <ul class="usa-nav__primary usa-accordion">
-              <li class="usa-nav__primary-item">
-                <a class="usa-nav__link usa-current" href="/">Home</a>
-              </li>
-              <li class="usa-nav__primary-item">
-                <a class="usa-nav__link" rel="noopener noreferrer" href="/filing/2023/">Filing</a>
-              </li>
-              <li class="usa-nav__primary-item">
-                <button type="button" class="usa-accordion__button usa-nav__link" aria-expanded="false" aria-controls="Data Browser">
-                  <span>Data Browser</span>
-                </button>
-                <ul id="Data Browser" class="usa-nav__submenu" hidden>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-browser/">Overview</a>
+            <ul className="usa-nav__primary usa-accordion">
+              {links.map(link => {
+                //let isActive = pathname.match(new RegExp('^' + link.href))
+                //if(link.href === '/') isActive = link.href === pathname
+
+                return (
+                  <li key={link.name} className="usa-nav__primary-item">
+                    {!link.submenu ? 
+                      <Link
+                        to={link.href}
+                        // className={
+                        //     isActive
+                        //     ? 'usa-nav__link usa-current'
+                        //     : 'usa-nav__link'
+                        // }
+                        //target={link.name === 'Filing' ? '_blank' : null}
+                        rel={
+                          link.name === 'Filing' ? 'noopener noreferrer' : null
+                        }
+                      >
+                        {link.name}
+                      </Link> :
+                      <>
+                        <button
+                          type="button"
+                          className="usa-accordion__button usa-nav__link"
+                          aria-expanded="false"
+                          aria-controls={link.name}
+                        >
+                          <span>{link.name}</span>
+                        </button>
+                        <ul id={link.name} className="usa-nav__submenu" hidden>
+                          {link.submenu.map(sublink => (<li key={sublink.name} className="usa-nav__submenu-item">
+                            {!sublink.href ? 
+                                <div className="subMenuHeading">{sublink.name}</div> :
+                                <Link
+                                  to={sublink.href}
+                                >
+                                  {sublink.name}
+                                </Link>
+                            }
+                          </li>)
+                          )}
+                        </ul>
+                      </>
+                    }
                   </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-browser/data/2021">Datasets</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-browser/maps/2021">Maps</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-browser/graphs/quarterly/">Graphs</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="usa-nav__primary-item">
-                <button type="button" class="usa-accordion__button usa-nav__link" aria-expanded="false" aria-controls="Data Publication">
-                  <span>Data Publication</span>
-                </button>
-                <ul id="Data Publication" class="usa-nav__submenu" hidden>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/">Overview</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <div class="subMenuHeading">Dynamic Datasets</div>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/modified-lar/">Modified LAR</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/dynamic-national-loan-level-dataset/">Dynamic Dataset</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <div class="subMenuHeading">Static Datasets</div>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/snapshot-national-loan-level-dataset/">Snapshot Datasets</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/one-year-national-loan-level-dataset/">One-Year Datasets</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/three-year-national-loan-level-dataset/">Three-Year Datasets</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/aggregate-reports/">MSA/MD Datasets</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/data-publication/disclosure-reports/">Disclosure Reports</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="usa-nav__primary-item">
-                <button type="button" class="usa-accordion__button usa-nav__link" aria-expanded="false" aria-controls="Tools">
-                  <span>Tools</span>
-                </button>
-                <ul id="Tools" class="usa-nav__submenu" hidden>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/tools/">Overview</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/tools/rate-spread">Rate Spread</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/tools/lar-formatting">LAR Formatting</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/tools/file-format-verification">File Verification</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/tools/check-digit">Check Digit</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="usa-nav__primary-item">
-                <button type="button" class="usa-accordion__button usa-nav__link" aria-expanded="false" aria-controls="Documentation">
-                  <span>Documentation</span>
-                </button>
-                <ul id="Documentation" class="usa-nav__submenu" hidden>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/documentation/">FAQs</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="https://cfpb.github.io/hmda-platform/#hmda-api-documentation" target="_blank">Developer APIs</a>
-                  </li>
-                  <li class="usa-nav__submenu-item">
-                    <a href="/updates-notes">Updates</a>
-                  </li>
-                </ul>
-              </li>
+                )
+              })}
             </ul>
+            {<SearchBar />}
           </nav>
         </div>
       </header>

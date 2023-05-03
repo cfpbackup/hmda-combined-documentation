991054197ecc7e0793751849db55fc1183a1fcb3
ojbravo <ojbravo@gmail.com>
2023-05-03 08:35:20 -0600

    Fixed Docusaurus Mobile Menu

--- src/theme/Navbar/Content/index.js
+++ src/theme/Navbar/Content/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Header from './Header'
+import { useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
+import NavbarMobileSidebarToggle from '@theme/Navbar/MobileSidebar/Toggle';
 
 const Navbar = () => {
+  const mobileSidebar = useNavbarMobileSidebar();
   return (
     <>
       <Header />
+      {!mobileSidebar.disabled && <NavbarMobileSidebarToggle />}
     </>
   )
 }

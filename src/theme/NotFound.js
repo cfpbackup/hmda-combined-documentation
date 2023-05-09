ed43e1ed52e0cbbd1e7c4de33261724ebdcbee94
michaeldremy <michaeldremy@gmail.com>
2023-05-09 11:11:11 -0700

    [404] Added a check on the 404 not found page to only display if the url includes /documentation/ in it. Fixes a bug where the 404 page would display when going back and forth from DS to FE.

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -6,72 +6,75 @@ import SearchBar from './SearchBar'
 
 export default function NotFound() {
   const [urlLocation, setUrllocation] = useState()
+  const [showNotFoundPage, setShowNotFoundPage] = useState(false)
 
   useEffect(() => {
     if (!window) return
     setUrllocation(window.location.pathname)
+
+    if (window.location.pathname.includes('/documentation/')) {
+      setShowNotFoundPage(true)
+    }
   }, [])
   return (
     <>
-      <PageMetadata
-        title={translate({
-          id: 'theme.NotFound.title',
-          message: 'Page Not Found',
-        })}
-      />
-      <Layout>
-        <main className='container margin-vert--xl not-found'>
-          <div className='row'>
-            <div className='col col--6 col--offset-3'>
-              <h1 className='hero__title'>
-                <Translate
-                  id='theme.NotFound.title'
-                  description='The title of the 404 page'
-                >
-                  Page Not Found
-                </Translate>
-              </h1>
-              <div>
-                <p style={{ fontWeight: 600 }}>
-                  URL: <span style={{ fontWeight: 500 }}>{urlLocation}</span>
-                </p>
-                <p>
-                  The above url was not able to direct you to a specific
-                  document.
-                </p>
+      {showNotFoundPage ? (
+        <>
+          <PageMetadata
+            title={translate({
+              id: 'theme.NotFound.title',
+              message: 'Page Not Found',
+            })}
+          />
+          <Layout>
+            <main className='container margin-vert--xl not-found'>
+              <div className='row'>
+                <div className='col col--6 col--offset-3'>
+                  <h1 className='hero__title'>
+                    <Translate
+                      id='theme.NotFound.title'
+                      description='The title of the 404 page'
+                    >
+                      Page Not Found
+                    </Translate>
+                  </h1>
+                  <div>
+                    <p style={{ fontWeight: 600 }}>
+                      URL:{' '}
+                      <span style={{ fontWeight: 500 }}>{urlLocation}</span>
+                    </p>
+                    <p>
+                      The above url was not able to direct you to a specific
+                      document.
+                    </p>
+                  </div>
+                  <p>
+                    <Translate
+                      id='theme.NotFound.p1'
+                      description='The first paragraph of the 404 page'
+                    >
+                      Please use the search bar below to search for the content
+                      you were looking for.
+                    </Translate>
+                  </p>
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignContent: 'center',
+                      marginBottom: '1rem',
+                    }}
+                  >
+                    <SearchBar />
+                  </div>
+                </div>
               </div>
-              <p>
-                <Translate
-                  id='theme.NotFound.p1'
-                  description='The first paragraph of the 404 page'
-                >
-                  Please use the search bar below to search for the content you
-                  were looking for.
-                </Translate>
-              </p>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignContent: 'center',
-                  marginBottom: '1rem',
-                }}
-              >
-                <SearchBar />
-              </div>
-              {/* <p>
-                <Translate
-                  id='theme.NotFound.p2'
-                  description='The 2nd paragraph of the 404 page'
-                >
-                  Please contact the owner of the site that linked you to the
-                  original URL and let them know their link is broken.
-                </Translate>
-              </p> */}
-            </div>
-          </div>
-        </main>
-      </Layout>
+            </main>
+          </Layout>
+        </>
+      ) : (
+        ''
+      )}
     </>
   )
 }
